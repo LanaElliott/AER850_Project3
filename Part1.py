50837a8c6c96f11787772858adc055ebbc209d93
LanaElliott <134975988+LanaElliott@users.noreply.github.com>
2023-12-18 21:24:01 -0500

    Part 1 working now
    yipee

--- Part1.py
+++ Part1.py
@@ -2,32 +2,44 @@ import cv2
 import numpy as np
 
 # Step 1: Read the image
-image = cv2.imread(r'C:\Users\sshak\Documents\GitHub\AER850_Project3\motherboard_image.JPEG') 
+image = cv2.imread(r'C:\Users\sshak\Documents\GitHub\AER850_Project3\motherboard_image.JPEG')
 
-# Step 2: Convert the image to grayscale
+# Step 2: Convert the image to grayscale and apply blur
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur_image = cv2.GaussianBlur(gray_image, (45, 45), 4)
 
 # Step 3: Thresholding
-_, thresholded_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+thresholded_image = cv2.adaptiveThreshold(blur_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 45, 5)
 
 # Step 4: Edge detection using Canny
-edges = cv2.Canny(thresholded_image, 50, 150)  # Adjust parameters as needed
+edges = cv2.Canny(thresholded_image, 1, 1)  
+edges = cv2.dilate(edges,None, iterations = 8)
 
 # Step 5: Contour detection
 contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Step 6: Filter out small contours based on area
-threshold_area = 2  # Adjust this value based on your requirements
-filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > threshold_area]
+# Step 6: Filter out contours based on area
+min_area_threshold = 10 
+max_area_threshold = 5000 
+
+filtered_contours = [cnt for cnt in contours if min_area_threshold < cv2.contourArea(cnt) < max_area_threshold]
 
 # Step 7: Create a mask
-mask = np.zeros_like(gray_image)
+mask = np.zeros_like(blur_image)
 
 # Step 8: Draw contours on the mask
-cv2.drawContours(mask, filtered_contours, -1, (255), thickness=cv2.FILLED)
+cv2.drawContours(mask, contours=[max(contours, key = cv2.contourArea)], contourIdx=-1, color=(255, 255, 255), thickness=cv2.FILLED)
 
 # Step 9: Use bitwise_and to extract the object
 masked_image = cv2.bitwise_and(image, image, mask=mask)
+
+# Set window names
+cv2.namedWindow('Original Image', cv2.WINDOW_NORMAL)
+cv2.namedWindow('Masked Image', cv2.WINDOW_NORMAL)
+
+# Resize windows
+cv2.resizeWindow('Original Image', 800, 600)
+cv2.resizeWindow('Masked Image', 800, 600)
 
 # Step 10: Display the results
 cv2.imshow('Original Image', image)
--- Part3.py
+++ Part3.py
@@ -1,15 +1,24 @@
 from PIL import Image
 from ultralytics import YOLO
+from PIL import Image
 
-# Load a pretrained YOLOv8n model
+ # Load a pretrained YOLOv8n model
 model = YOLO('yolov8n.pt')
 
-# Run inference on 'bus.jpg'
-results = model('bus.jpg')  # results list
+# # Run inference on 'bus.jpg'
+# results = model(r'C:\Users\sshak\Documents\GitHub\AER850_Project3\data')  # results list
 
-# Show the results
-for r in results:
-    im_array = r.plot()  # plot a BGR numpy array of predictions
-    im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-    im.show()  # show image
-    im.save('results.jpg')  # save image
+# # Show the results
+# for r in results:
+#     im_array = r.plot()  # plot a BGR numpy array of predictions
+#     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+#     im.show()  # show image
+#     im.save('results.jpg')  # save image
+
+# Evaluate the model
+for image_path in [r'C:/Users/sshak/Documents/GitHub/AER850_Project3/data/evaluation/ardmega.jpg',
+                    r'C:/Users/sshak/Documents/GitHub/AER850_Project3/data/evaluation/arduino.jpg',
+                    r'C:/Users/sshak/Documents/GitHub/AER850_Project3/data/evaluation/rasppi.jpg']:
+    img = Image.open(image_path)
+    results = model.predict(img, save=True)
+    print(results)  # print results to stdout
